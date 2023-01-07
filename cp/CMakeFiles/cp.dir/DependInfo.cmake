f6e2b83052c3770e32860928fcf8ea4c5f475dcd
Fisben <shukaylo.ekaterina@gmail.com>
2023-01-07 09:53:10 +0300

    Add new files for cp

---
+++ cp/CMakeFiles/cp.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fisben/OS/cp/main.cpp" "cp/CMakeFiles/cp.dir/main.cpp.o" "gcc" "cp/CMakeFiles/cp.dir/main.cpp.o.d"
+  "/home/fisben/OS/cp/src/ListAllocator.cpp" "cp/CMakeFiles/cp.dir/src/ListAllocator.cpp.o" "gcc" "cp/CMakeFiles/cp.dir/src/ListAllocator.cpp.o.d"
+  "/home/fisben/OS/cp/src/N2Allocator.cpp" "cp/CMakeFiles/cp.dir/src/N2Allocator.cpp.o" "gcc" "cp/CMakeFiles/cp.dir/src/N2Allocator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/fisben/OS/cp
+# Build directory: /home/fisben/OS/cp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
